9b17009d28416d8beda5ebfdef894103a159717b
aizjicod <98921366+aizjicod@users.noreply.github.com>
2022-06-13 18:54:28 -0400

    fix linters on js file

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-const btn = document.querySelector(".menu-button");
-const menu = document.querySelector(".menu-mobile");
-const li = document.querySelectorAll(".menu-mobile li")
-let listItem = Array.from(li);
-console.log(listItem)
-let menuTgl = ()=> { menu.classList.toggle("visibility"); }
-btn.addEventListener("click",menuTgl);
-listItem.forEach(child => {
-
-
-
-    
-    child.addEventListener("click",menuTgl);  
-});
+const btn = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu-mobile');
+const li = document.querySelectorAll('.menu-mobile li');
+const listItem = Array.from(li);
+const menuTgl = () => { menu.classList.toggle('visibility'); };
+btn.addEventListener('click', menuTgl);
+listItem.forEach((child) => { child.addEventListener('click', menuTgl); });
