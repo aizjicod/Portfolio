c3d0d49ff5c74a255ae31b3933006a6fbcdb25e5
ch4ne5teban <ca1505pr@gmail.com>
2022-06-14 02:40:58 +0530

    Add js variables const btn menu and li

---
+++ script.js
@@ -0,0 +1,4 @@
+const btn = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu-mobile");
+const li = document.querySelectorAll(".menu-mobile li")
+let listItem = Array.from(li);
