b12049b381b851d0fd05a4a34104886d89b3271e
renercrows <ungredda95@gmail.com>
2022-06-13 17:55:56 -0400

    Finish JavaScript Code

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const btn = document.querySelector(".menu-button");
 const menu = document.querySelector(".menu-mobile");
 const li = document.querySelectorAll(".menu-mobile li")
 let listItem = Array.from(li);
+console.log(listItem)
+let menuTgl = ()=> { menu.classList.toggle("visibility"); }
+btn.addEventListener("click",menuTgl);
+listItem.forEach(child => {
+    child.addEventListener("click",menuTgl);
+});
