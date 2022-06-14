f7596a5b6a25342dc4826ddd9b72888e84d7706b
aizjicod <98921366+aizjicod@users.noreply.github.com>
2022-06-13 21:46:31 -0400

    fix navbar menu

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const btn = document.querySelector('.menu-button');
+const btn = document.querySelector('.hamburguer-btn');
 const menu = document.querySelector('.menu-mobile');
 const li = document.querySelectorAll('.menu-mobile li');
+const logo = document.querySelector('.logo-placeholder');
+const header = document.querySelector('.header');
 const listItem = Array.from(li);
-const menuTgl = () => { menu.classList.toggle('visibility'); };
+const menuTgl = () => { 
+    menu.classList.toggle('visibility'); 
+    logo.classList.toggle('visibility');
+    btn.classList.toggle('active');
+    header.classList.toggle('active')};
+
 btn.addEventListener('click', menuTgl);
 listItem.forEach((child) => { child.addEventListener('click', menuTgl); });
